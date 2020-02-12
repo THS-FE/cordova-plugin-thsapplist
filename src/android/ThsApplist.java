c64b1247bfd57fa359e4de0eb2ca04a4566d0670
liuyx <867947704@qq.com>
2020-02-12 18:03:29 +0800

    完善获取应用列表信息操作

--- src/android/ThsApplist.java
+++ src/android/ThsApplist.java
@@ -1,11 +1,20 @@
 package cn.com.ths.applist;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -15,18 +24,39 @@ public class ThsApplist extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("getAppList")) {
-            String message = args.getString(0);
-            this.getAppList(message, callbackContext);
+            this.getAppList(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void getAppList(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+    private void getAppList(CallbackContext callbackContext){
+        try {
+        List<PackageInfo> packages = cordova.getActivity().getPackageManager().getInstalledPackages(0);
+        JSONArray appArr = new JSONArray();
+        for (int i = 0; i < packages.size(); i++) {
+            JSONObject appObj =new JSONObject();
+            PackageInfo packageInfo = packages.get(i);
+
+            String  appName = packageInfo.applicationInfo.loadLabel(cordova.getActivity().getPackageManager()).toString();
+            appObj.put("appName",appName);
+            String  packageName = packageInfo.packageName;
+            appObj.put("packageName",packageName);
+            String  versionName = packageInfo.versionName;
+            appObj.put("versionName",versionName);
+            int versionCode = packageInfo.versionCode;
+            appObj.put("versionCode",versionCode);
+
+            //Only display the non-system app info  
+
+            if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+                appArr.put(appObj);//如果非系统应用，则添加至appList  
+            }
         }
+            callbackContext.success(appArr);
+        }catch (JSONException e){
+            callbackContext.error(e.getMessage());
+        }
+
     }
 }
